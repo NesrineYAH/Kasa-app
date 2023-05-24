3b080f04fa451657ee6b80c298c5c273f7d9e9c7
NesrineYAH <nesrineyahoum@gmail.com>
2023-05-24 23:18:47 +0200

    Mise à jours

--- src/components/Carrousel/Carrousel.js
+++ src/components/Carrousel/Carrousel.js
@@ -2,19 +2,19 @@ import React, { useState } from "react";
 import left from "../../assets/images/carrousel/flech-left.png";
 import right from "../../assets/images/carrousel/flech-right.png";
 
-const Carrousel = ({ slides }) => {
-  const [Current, setCurrent] = useState(0); //j'ai initialié l'index du premier slide à 0
+export default function Carrousel({ slides }) {
+  const [current, setCurrent] = useState(0); //j'ai initialisé l'index du premier slide à 0
   const length = slides.length; // longueur du tableau de slides
 
   /**Function pour l'image precedente */
   const nextImage = () => {
-    setCurrent(Current === length - 1 ? 0 : Current + 1); // on début avec le premier slide quand on arrive au dernier
+    setCurrent(current === length - 1 ? 0 : current + 1); // on début avec le premier slide quand on arrive au dernier
   };
   /**Function pour l'image suivante */
   const prevImage = () => {
-    setCurrent(Current === 0 ? length - 1 : Current - 1); // on repart au dernier slide quand on est au premier
+    setCurrent(current === 0 ? length - 1 : current - 1); // on repart au dernier slide quand on est au premier
   };
-
+ 
   return (
     <section className="Carrousel__Contanier">
       {length > 1 && (
@@ -22,7 +22,7 @@ const Carrousel = ({ slides }) => {
           src={left} //Affichage des flèches seulement si length > 1
           alt="gauche"
           onClick={prevImage}
-          className="leftArrow"
+          className="left-Arrow"
         />
       )}
       {length > 1 && (
@@ -30,27 +30,25 @@ const Carrousel = ({ slides }) => {
           src={right}
           alt="droite"
           onClick={nextImage}
-          className="rightArrow"
+          className="right-Arrow"
         />
       )}
 
-      {slides.map((image, index) => {
+      {slides.map((slide, index) => {
         <div
           key={index}
-          className={Current === index ? "slider active" : "slider"}
+          className={current === index ? "slider active" : "slider"}
         >
-          {index === Current && (
-            <img src={image} alt="img-appartement" className="slide__image" />
+          {index === current && (
+            <img src={slide} alt="img-appartement" className="slide__image" />
           )}
-          {index === Current && length > 1 && (
+          {index === current && length > 1 && (
             <span className="slider__number">
-              {/* {Current + 1}/{length} */}
+              {current + 1}/{length}
             </span>
           )}
         </div>;
       })}
     </section>
   );
-};
-
-export default Carrousel;
+}
