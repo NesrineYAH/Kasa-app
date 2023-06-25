e74a434f33091810856d8e10368f6560d7ebef2f
NesrineYAH <nesrineyahoum@gmail.com>
2023-06-25 20:37:39 +0200

    Mise à jours

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,11 +1,11 @@
 import homeBanner from "../../assets/images/banner/homeBanner.png";
 
-
 const Banner = ({ image, texte }) => {
   return (
     <div className="Banner__conteneur">
       <h1 className="Banner__h1">Chez vous, partout et ailleurs</h1>
       <img className="Banner__img" src={homeBanner} alt="banner kasa" />
+      {/* <div className="Banner__sombre"></div> */}
     </div>
   );
 };
