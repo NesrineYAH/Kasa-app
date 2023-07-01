0838e9bf6730a6e50e6b59acbf80d78c6ea02124
NesrineYAH <nesrineyahoum@gmail.com>
2023-07-01 15:17:33 +0200

    Mise à jour

--- src/components/AboutBanner/AboutBanner.js
+++ src/components/AboutBanner/AboutBanner.js
@@ -4,7 +4,11 @@ import aboutBanner from "../../assets/images/banner/aboutBanner.png";
 const AboutBanner = () => {
   return (
     <div className="AboutBanner__conteneur">
-      <img className="Banner__img" src={aboutBanner} alt="About-page" />
+      <img
+        className="BannerContainer__img"
+        src={aboutBanner}
+        alt="About-page"
+      />
     </div>
   );
 };
